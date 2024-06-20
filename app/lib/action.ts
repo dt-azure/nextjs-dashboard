724b3ecb45bd35ede3c5ead95a06067b1fab3594
Trung Nguyen <dtrung2702@gmail.com>
2024-06-20 17:31:39 +0700

    third commit

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -3,21 +3,29 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string()
-})
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+});
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export const createInvoice = async (formData: FormData) => {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export const createInvoice = async (prevState: State, formData: FormData) => {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
@@ -29,13 +37,25 @@ export const createInvoice = async (formData: FormData) => {
     //     status: formData.get('status'),
     // }
 
-    const amountInCents = amount * 100
-    const date = new Date().toISOString().split('T')[0]
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
 
-    await sql`
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date) VALUES
         (${customerId}, ${amountInCents}, ${status}, ${date})
     `
+    } catch (err) {
+        return { message: 'Error.' }
+    }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
@@ -50,12 +70,42 @@ export const updateInvoice = async (id: string, formData: FormData) => {
 
     const amountInCents = amount * 100
 
-    await sql`
+    try {
+        await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
     `
+    } catch (err) {
+        return { message: 'Error.' }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function authenticate(prevState: string | undefined,
+    formData: FormData) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
 }
